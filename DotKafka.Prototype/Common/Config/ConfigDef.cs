1924876af100b6df1508fc84dd543f175156f50d
Alex Gonzalez <andritogv@gmail.com>
2015-12-09 01:12:19 -0600

    beginning config classes

---
+++ DotKafka.Prototype/Common/Config/ConfigDef.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+
+namespace DotKafka.Prototype.Common.Config {
+    public class ConfigDef {
+        private static readonly object NoDefaultValue = string.Empty;
+        private readonly Dictionary<string, ConfigKey> _configKeys = new Dictionary<string, ConfigKey>();
+
+        public HashSet<string> Names() {
+            return new HashSet<string>(_configKeys.Keys);
+        }
+
+        public enum Type {
+            Boolean,
+            String,
+            Int,
+            Short,
+            Long,
+            Double,
+            List,
+            Class,
+            Password
+        }
+
+        public enum Importance {
+            High,
+            Medium,
+            Low
+        }
+
+        public interface IValidator {
+            void EnsureValid(string name, object o);
+        }
+
+        private class ConfigKey {
+            public string Name { get; set; }
+            public Type Type { get; set; }
+            public string Documentation { get; set; }
+            public object DefaultValue { get; set; }
+            public IValidator Validator { get; set; }
+            public Importance Importance { get; set; }
+
+            public ConfigKey(string name, Type type, object defaultValue, IValidator validator, Importance importance,
+                string documentation) {
+                Name = name;
+                Type = type;
+                DefaultValue = defaultValue;
+                Validator = validator;
+                Importance = importance;
+                Validator?.EnsureValid(name, defaultValue);
+                Documentation = documentation;
+            }
+
+            public bool HasDefault() {
+                return DefaultValue != NoDefaultValue;
+            }
+        }
+    }
+}
---
+++ DotKafka.Prototype/Common/Config/Types/Password.cs
@@ -0,0 +1,33 @@
+﻿namespace DotKafka.Prototype.Common.Config.Types {
+    public class Password {
+        public static string Hidden = "[hidden]";
+        private readonly string _value;
+
+        public Password(string value) {
+            _value = value;
+        }
+
+        public int HashCode() {
+            return GetHashCode();
+        }
+
+        public override int GetHashCode() {
+            return _value.GetHashCode();
+        }
+
+        public override bool Equals(object obj) {
+            if (!(obj is Password))
+                return false;
+            var other = (Password) obj;
+            return _value.Equals(other._value);
+        }
+
+        public override string ToString() {
+            return Hidden;
+        }
+
+        public string Value() {
+            return _value;
+        }
+    }
+}
